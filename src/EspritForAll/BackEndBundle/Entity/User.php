855bb3f72ff8e9e8142a9a619517132417ef7c98
farhatyacine <yacine.farhat@esprit.tn>
2017-11-19 19:17:16 +0100

    first commit

--- src/EspritForAll/BackEndBundle/Entity/User.php
+++ src/EspritForAll/BackEndBundle/Entity/User.php
@@ -61,5 +61,20 @@ class User extends BaseUser
         parent::__construct();
     }
 
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 }
 
