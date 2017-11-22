72132eec5adb9c7827b466ce249ebe3e9765dfab
firashamila33 <firas.hamila@esprit.tn>
2017-11-22 19:18:17 +0100

    fixing some issues

--- src/FrontEndBundle/Controller/RestaurentSingleProductController.php
+++ src/FrontEndBundle/Controller/RestaurentSingleProductController.php
@@ -10,10 +10,18 @@ namespace FrontEndBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
-class RestaurentSingleProductController extends  Controller
+class RestaurentSingleProductController extends Controller
 {
     public function indexAction()
     {
         return $this->render('FrontEndBundle:Restaurent:SingleProduct.html.twig');
+    }
+
+    public function GetMealAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $menu = $em->getRepository("EspritForAllBackEndBundle:Menu")->findOneBy(array('id' => $id));
+
+        return $this->render('FrontEndBundle:Restaurent:SingleProduct.html.twig', array('menu' => $menu));
     }
 }
